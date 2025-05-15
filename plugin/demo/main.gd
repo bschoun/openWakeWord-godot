ce2f593a13b7f26c3b9f5724b0bb07eb6b805d68
bschoun <breawnschoun@gmail.com>
2025-05-15 15:26:41 -0600

    Added 5 different commands, removed names. Updated main.gd to use commands.

--- plugin/demo/main.gd
+++ plugin/demo/main.gd
@@ -4,7 +4,7 @@ extends OpenWakeWord
 @export var button : Button
 
 # TODO: replace with array of names of your models in assets folder
-var models = Array(["down.onnx", "up.onnx", "sit.onnx", "galaxy.onnx"])
+var models = Array(["good_dog.onnx", "lay_down.onnx", "stand_up.onnx", "sit_down.onnx", "come_here.onnx"])
 
 func _ready() -> void:
 	super()
@@ -23,7 +23,7 @@ func _on_start_stop_button_toggled(toggled_on: bool) -> void:
 	if toggled_on:
 		# Choose a chunk size that suits your needs. Must be > 400 and must be a multiple of 80
 		# Default for most examples is 1280. I use 720 to detect shorter phrases.
-		var chunk_size = 720 
+		var chunk_size = 800 
 		start_detection(models, chunk_size)
 		button.text = "Stop"
 	else:
